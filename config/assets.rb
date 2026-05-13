fabdc41f077a646bbf269733077f44c434fbe33a
Petter <petter@digidemlab.org>
2026-05-13 11:06:11 +0200

    Fix entrypoint

--- config/assets.rb
+++ config/assets.rb
@@ -2,6 +2,3 @@ base_path = File.expand_path("..", __dir__)
 
 Decidim::Webpacker.register_path("#{base_path}/app/packs")
 Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/modules/ui_tweaks")
-Decidim::Webpacker.register_entrypoints(
-  decidim_ui_tweaks: "#{base_path}/app/packs/entrypoints/decidim_ui_tweaks.js"
-)
